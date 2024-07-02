49e489444581d17f227eeb5ced391ea4a549f830
rbrenor2 <rbrenorios@gmail.com>
2024-07-02 07:41:46 +0100

    Initial Commit

---
+++ timeshield/timeshieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeshieldApp.swift
+//  timeshield
+//
+//  Created by Breno R R on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct timeshieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ timeshieldTests/timeshieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  timeshieldTests.swift
+//  timeshieldTests
+//
+//  Created by Breno R R on 02/07/2024.
+//
+
+import Testing
+@testable import timeshield
+
+struct timeshieldTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
